a82046634d2e144df0af0503bb1c92f4f72dd76c
Paul Sydney <xtrycatchx@gmail.com>
2018-04-07 16:20:07 +0800

    minor

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import { serverPort } from './config'
 
 const app = express()
 
-app.use([express.json(), express.urlencoded({ extended:true })])
+app.use([express.json(), express.urlencoded({ extended: true })])
 app.use(methodOverride())
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   console.log('Time:', Date.now())
   next()
 })
 
 app.use('/api', apiRouter)
 
-app.use(function(req, res, next){
-  res.status(404).send('404', { url: req.originalUrl })
+app.use((req, res, next) => {
+  res.status(404).send({ url: req.originalUrl })
 })
 
 app.listen(serverPort)
