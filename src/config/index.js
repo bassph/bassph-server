b4f024bb96fdf453112b2cab6b5ea1e1d2153834
Allan Crisostomo <allan.crisostomo@whitecloak.com>
2018-04-12 23:07:08 +0800

    Fix config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,7 @@
-const CONFIG = {
+module.exports = {
   serverPort: process.env.SERVER_PORT,
   mongoUrl: 'mongodb://localhost:27017/bassdb',
   dbName: 'bassdb',
   lookupApiUrl: 'https://ap1.unwiredlabs.com/v2/process.php',
   lookupToken: '9ca648def92237'
 }
-
-export default CONFIG
