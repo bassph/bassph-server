aaf3641cbfbb7accb438a3db04c0f72154dd40c1
Paul Sydney <xtrycatchx@gmail.com>
2018-06-18 23:35:05 +0800

    bug fix for csv stream

--- src/controllers/mappers/ScanResult.js
+++ src/controllers/mappers/ScanResult.js
@@ -71,13 +71,68 @@ export function transform(req) {
     return cleanObject(scanResult)
 }
 
-export function transformToCsv(scanResults) {
-    // TODO: Implement streaming
-    try {
-        const parser = new Parser(scanResultCsvFieldMapping);
-        const csv = parser.parse(scanResults);
-        return csv
-    } catch (err) {
-        console.error(err);
+export function transformToCsv(scanResult) {
+    // // TODO: Implement streaming
+    // try {
+    //     const parser = new Parser(scanResultCsvFieldMapping);
+    //     const csv = parser.parse(scanResults);
+    //     return csv
+    // } catch (err) {
+    //     console.error(err);
+    // }
+
+    // TODO convert to yyyy-mm-dd hh:mm
+    let date = scanResult.location.elapsedRealtimeNanos;
+
+    return {
+        TESTNUMBER: scanResult._id.toString(),
+        BANDWIDTH: scanResult.bandwidth,
+        OPERATOR: scanResult.operator,
+        SIGNAL: scanResult.signal,
+        SIGNALBARS: scanResult.signalBars,
+        CONNECTIVITY_available: scanResult.connectivity.available,
+        CONNECTIVITY_detailedState: scanResult.connectivity.detailedState,
+        CONNECTIVITY_extraInfo: scanResult.connectivity.extraInfo ? scanResult.connectivity.extraInfo.replace(/&quot;/g,'') : 'none',
+        CONNECTIVITY_failOver: scanResult.connectivity.failover,
+        CONNECTIVITY_roaming: scanResult.connectivity.roaming,
+        CONNECTIVITY_state: scanResult.connectivity.state,
+        CONNECTIVITY_subType: scanResult.connectivity.subType,
+        CONNECTIVITY_subTypeName: scanResult.connectivity.subTypeName,
+        CONNECTIVITY_type: scanResult.connectivity.type,
+        CONNECTIVITY_typeName: scanResult.connectivity.typeName,
+        DEVICE_manufacturer: scanResult.device.manufacturer,
+        DEVICE_model: scanResult.device.model,
+        DEVICE_name: scanResult.device.name,
+        DEVICE_release: scanResult.device.release,
+        LOCATION_mAccuracy: scanResult.location.accuracy,
+        LOCATION_mAltitude: scanResult.location.altitude,
+        LOCATION_mDistance: scanResult.location.distance,
+        LOCATION_mElapsedRealtimeNanos: scanResult.location.elapsedRealtimeNanos,
+        LOCATION_mHasAccuracy: scanResult.location.hasAccuracy,
+        LOCATION_mHasAltitude: scanResult.location.hasAltitude,
+        LOCATION_mHasBearing: scanResult.location.hasBearing,
+        LOCATION_mHasSpeed: scanResult.location.hasSpeed,
+        LOCATION_mInitialBearing: scanResult.location.initialBearing,
+        LOCATION_mIsFromMockProvider: scanResult.location.isFromMockProvider,
+        LOCATION_mLat1: scanResult.location.lat1,
+        LOCATION_mLat2: scanResult.location.lat2,
+        LOCATION_mLatitude: scanResult.location.latitude,
+        LOCATION_mLon1: scanResult.location.lon1,
+        LOCATION_mLon2: scanResult.location.lon2,
+        LOCATION_mLongitude: scanResult.location.longitude,
+        LOCATION_mProvider: scanResult.location.provider,
+        LOCATION_mSpeed: scanResult.location.speed,
+        NETWORK_cid: scanResult.cellTower? scanResult.cellTower.cid : 'none',
+        NETWORK_lac: scanResult.cellTower? scanResult.cellTower.lac : 'none',
+        NETWORK_mcc: scanResult.cellTower? scanResult.cellTower.mcc : 'none',
+        NETWORK_mnc: scanResult.cellTower? scanResult.cellTower.mnc : 'none',
+        CELLTOWER_lat: scanResult.cellTower? scanResult.cellTower.lat : 'none',
+        CELLTOWER_lon: scanResult.cellTower? scanResult.cellTower.lon : 'none',
+        CELLTOWER_address: scanResult.cellTower? scanResult.cellTower.address : 'none',
+        CELLTOWER_accuracy: scanResult.cellTower? scanResult.cellTower.address : 'none',
+        CELLTOWER_lookupStatus: scanResult.cellTower? scanResult.cellTower.status: 'none',
+        VERSION: scanResult.version,
+        LOCATION_epochTime: date, //scanResult.location.elapsedRealtimeNanos,
+        MOOD: scanResult.mood
     }
 }
--- src/controllers/scanResultController.js
+++ src/controllers/scanResultController.js
@@ -1,5 +1,6 @@
 import autoBind from 'auto-bind'
 import * as ScanResultMapper from './mappers/ScanResult'
+import * as csv from 'csv'
 
 class ScanResultController {
 	constructor({ storeScanResult, showScanResults }) {
@@ -24,30 +25,39 @@ class ScanResultController {
 	get(req, res) {
 		res.contentType('text/csv')
 		res.header('Content-Disposition', 'attachment; filename=scan_results.csv')
-		
-		//TODO
-		res.write('>>>>> HEADERS HERE');
-		
-		res.write('\r\n')
-		let prevChunk = null;
+
+		res.flushHeaders();
+
 		const stream = this.showScanResults.executeAsStream({ afterEpoch: req.query.afterEpoch })
-		stream.on('error', err => {
-			console.error(err)
-			res.end('\r\n');
-		})
-		stream.on('data', doc => {
-			if (prevChunk) {
-				res.write(Object.values(prevChunk).join(','));
-				res.write('\r\n')
-			}
-			prevChunk = doc;
-		})
-		stream.on('end', () => {
-			if (prevChunk) {
-				res.write(Object.values(prevChunk).join(','));
-			}
-			res.end('\r\n');
-		});
+		stream
+			.pipe(csv.transform(ScanResultMapper.transformToCsv))
+			.pipe(csv.stringify({ header: true }))
+			.pipe(res)
+
+		//TODO
+		// res.write('>>>>> HEADERS HERE');
+
+		// res.write('\r\n')
+		// let prevChunk = null;
+		// const stream = this.showScanResults.executeAsStream({ afterEpoch: req.query.afterEpoch })
+		// stream.on('error', err => {
+		// 	console.error(err)
+		// 	res.end('\r\n');
+		// })
+		// stream.on('data', doc => {
+		// 	if (prevChunk) {
+		// 		res.write(Object.values(prevChunk).join(','));
+		// 		res.write('\r\n')
+		// 	}
+		// 	console.log("\n\n\n\n\n",doc,"\n\n\n\n\n")
+		// 	prevChunk = doc;
+		// })
+		// stream.on('end', () => {
+		// 	if (prevChunk) {
+		// 		res.write(Object.values(prevChunk).join(','));
+		// 	}
+		// 	res.end('\r\n');
+		// });
 		/**this.showScanResults.execute({ afterEpoch: req.query.afterEpoch })
 			.subscribe(scanResults => {
 				if (req.query._format == 'csv') {
